31fa7ac667228929911050a1e521f2f0a76d3048
TechHog <77560013+TechHog8984@users.noreply.github.com>
2022-03-13 16:51:58 +0000

    Update example.lua

--- example.lua
+++ example.lua
@@ -5,22 +5,22 @@ local Section1 = Gui:CreateSection{'Section1'}
 local Section2 = Gui:CreateSection{'Section2'}
 local Settings = Gui:CreateSection{'Settings'}
 
-local Toggle = nil
-Toggle = Section1:Toggle{'Stuff', function(status)print('Stuff: ', status) end}
+local Toggle;Toggle = Section1:Toggle{'Stuff', function(status)print('Stuff: ', status) end}
 
-local Button = nil
-Button = Section1:Button{'Click me!', function()print'Button clicked!'end}
+local Button;Button = Section1:Button{'Click me!', function()print'Button clicked!'end}
+Section1:Button{'Or me!', function()print'Other button clicked!'end}
 
-local Bind = nil
-Bind = Section1:Keybind{'Bind', function()print'Bind key pressed'end, Enum.KeyCode.X}
+local Bind;Bind = Section1:Keybind{'Bind', function()print'Bind key pressed'end, Enum.KeyCode.X}
 
 Section1:Label{'Label1'}
-Section1:Label{'Label2'}
-Section1:Label{'Label3'}
-Section1:Label{'Label4'}
 
-local Box = nil
-Box = Section2:Textbox{'BOX', function(ep)print('Box, Text: ', Box.Text, 'Enter pressed: ', ep)end}
+local Box;Box = Section2:Textbox{'BOX', function(ep)print('Box, Text: ', Box.Text, 'Enter pressed: ', ep)end}
+local Dropdown;Dropdown = Section2:Dropdown{'Dropdown', {'amog1', 'amog2', 'us1', 'us2'}, nil, function(option)print('Dropdown: ', option)end}
 
-local Close = nil
-Close = Settings:Button{'Close', Gui.Close}
+local Close;Close = Settings:Button{'Close', Gui.Destroy}
+
+local ThemeDropdownOptions = {}
+for Theme, V in next, Gui.Themes do
+  table.insert(ThemeDropdownOptions, Theme)
+end
+Settings:Dropdown{'Theme', ThemeDropdownOptions, 'Dark', function(Option) Gui:ChangeTheme(Option) end}
